8456942d322d1883f6881b336f4d7969d9401a62
Andraelja <eljapramaandra@gmail.com>
2025-09-07 01:56:30 +0700

    commit pertama

---
+++ controllers/BukuController.js
@@ -0,0 +1,145 @@
+const express = require("express");
+
+const prisma = require("../prisma/client");
+
+const findBuku = async (req, res) => {
+  try {
+    const buku = await prisma.buku.findMany({
+      select: {
+        id: true,
+        judul: true,
+        penulis: true,
+        penerbit: true,
+        tahun: true,
+        kategori: true,
+        stok: true,
+      },
+      orderBy: {
+        id: "desc",
+      },
+    });
+
+    return res.status(200).send({
+      success: true,
+      message: "Berhasil mendapatkan semua buku!",
+      data: buku,
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "Terjadi kesalahan memuat data!",
+    });
+  }
+};
+
+const createBuku = async (req, res) => {
+  try {
+    const buku = await prisma.buku.create({
+      data: {
+        judul: req.body.judul,
+        penulis: req.body.penulis,
+        penerbit: req.body.penerbit,
+        tahun: req.body.tahun ? Number(req.body.tahun) : null,
+        kategori: req.body.kategori,
+        stok: req.body.stok ? Number(req.body.stok) : 0,
+      },
+    });
+
+    res.status(201).send({
+      success: true,
+      message: "Berhasil menambahkan data!",
+      data: buku,
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "Terjadi kesalahan internal!",
+    });
+  }
+};
+
+const findBukuById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const buku = await prisma.buku.findUnique({
+      where: {
+        id: Number(id),
+      },
+      select: {
+        id: true,
+        judul: true,
+        penulis: true,
+        penerbit: true,
+        tahun: true,
+        kategori: true,
+        stok: true,
+      },
+    });
+
+    res.status(200).send({
+      success: true,
+      message: `Buku dengan id ${id} ditemukan!`,
+      data: buku,
+    });
+  } catch (error) {
+    return res.status(500).send({
+      success: false,
+      message: "Terjadi kesalahan internal!",
+    });
+  }
+};
+
+const updateBuku = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const buku = await prisma.buku.update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        judul: req.body.judul,
+        penulis: req.body.penulis,
+        penerbit: req.body.penerbit,
+        tahun: req.body.tahun ? Number(req.body.tahun) : null,
+        kategori: req.body.kategori,
+        stok: req.body.stok ? Number(req.body.stok) : 0,
+      },
+    });
+
+    return res.status(200).send({
+      success: true,
+      message: "Berhasil memperbarui data!",
+      data: buku,
+    });
+  } catch (error) {
+    return res.status(500).send({
+      success: false,
+      message: "Terjadi kesalahan internal!",
+    });
+  }
+};
+
+const deleteBuku = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const buku = await prisma.buku.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    res.status(200).send({
+      success: true,
+      message: "Berhasil menghapus data!",
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "Terjadi kesalahan internal!",
+    });
+  }
+};
+
+module.exports = { findBuku, createBuku, findBukuById, updateBuku, deleteBuku };
---
+++ routes/index.js
@@ -0,0 +1,39 @@
+//import express
+const express = require("express");
+
+//init express router
+const router = express.Router();
+
+// import verify toker
+const verifyToken = require("../middlewares/auth");
+
+//import register controller
+const registerController = require("../controllers/RegisterController");
+const loginController = require("../controllers/LoginController");
+const userController = require("../controllers/UserController");
+const bukuController = require("../controllers/BukuController")
+
+//import validate register
+const { validateRegister, validateLogin } = require("../utils/validators/auth");
+const { validateUser } = require('../utils/validators/user')
+
+// Auth
+router.post("/register", validateRegister, registerController.register);
+router.post("/login", validateLogin, loginController.login);
+
+// user
+router.get("/admin/users", verifyToken, userController.findUsers);
+router.post("/admin/users", verifyToken, validateUser, userController.createUser);
+router.get("/admin/users/:id", verifyToken, userController.findUserById);
+router.put("/admin/users/:id", verifyToken, validateUser, userController.updateUser);
+router.delete("/admin/users/:id", verifyToken, userController.deleteUser);
+
+// buku
+router.get("/admin/buku", bukuController.findBuku);
+router.post("/admin/buku", bukuController.createBuku);
+router.get("/admin/buku/:id", bukuController.findBukuById);
+router.put("/admin/buku/:id", bukuController.updateBuku);
+router.delete("/admin/buku/:id", bukuController.deleteBuku);
+
+//export router
+module.exports = router;
