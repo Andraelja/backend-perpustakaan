ea8d2c3dfe406207672bdbd51981997a16df3137
Andraelja <eljapramaandra@gmail.com>
2025-10-04 22:53:11 +0700

    tambahan field deskripsi dan foto

--- prisma/migrations/20250906173952_buku/migration.sql
+++ prisma/migrations/20250906173952_buku/migration.sql
@@ -6,6 +6,8 @@ CREATE TABLE `buku` (
     `penerbit` VARCHAR(255) NULL,
     `tahun` INTEGER NULL,
     `kategori` VARCHAR(255) NULL,
+    `deskripsi` VARCHAR(255) NULL,
+    `foto` VARCHAR(255) NULL,
     `stok` INTEGER NOT NULL DEFAULT 0,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
