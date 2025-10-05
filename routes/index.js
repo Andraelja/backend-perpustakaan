604d00f8c4eb69b044430dc0cb10467e667dece5
Andraelja <eljapramaandra@gmail.com>
2025-10-06 01:09:47 +0700

    perbaikan seditkit

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ router.post("/register", validateRegister, registerController.register);
 router.post("/login", validateLogin, loginController.login);
 
 // user
-router.get("/admin/users", verifyToken, userController.findUsers);
+router.get("/admin/users", userController.findUsers);
 router.post("/admin/users", verifyToken, validateUser, userController.createUser);
 router.get("/admin/users/:id", verifyToken, userController.findUserById);
 router.put("/admin/users/:id", verifyToken, validateUser, userController.updateUser);
